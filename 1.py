81b799d514d364e3e1a2923257f40054ca3ec5fb
mike_karni <3923499@mail.ru>
2022-07-15 07:01:14 +0300

    FirstTaskDOne

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 '''Задайте список. Напишите программу, которая определит,
 присутствует ли в заданном списке строк некое число.'''
 
-import main as CheckList
+import main as Function1
 
-print(main.CheckList([1,2,3,4,5,6,7,8],0))
+print(Function1.CheckList([1,2,3,4,5,6,7,8],0))
+print(Function1.CheckList([1,2,3,4,5,6,7,8],1))
+print(Function1.CheckList([1,2,3,4,5,6,7,8],7))
+print(Function1.CheckList([1,2,3,4,5,6,7,8],10))
