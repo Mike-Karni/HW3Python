37b1b9b62c1222dc8eadf9b66ae6dc75328c4ad2
mike_karni <3923499@mail.ru>
2022-07-15 07:31:21 +0300

    SecondTaskProblen

--- 1.py
+++ 1.py
@@ -2,7 +2,6 @@
 присутствует ли в заданном списке строк некое число.'''
 
 import main as Function1
-
 print(Function1.CheckList([1,2,3,4,5,6,7,8],0))
 print(Function1.CheckList([1,2,3,4,5,6,7,8],1))
 print(Function1.CheckList([1,2,3,4,5,6,7,8],7))
